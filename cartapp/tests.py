c49f98e53c9c49d6e5ef50b1f9b8f8f5a8f8d921
YungJie <j86121100@gmail.com>
2020-07-13 17:44:51 +0800

    6

--- cartapp/tests.py
+++ cartapp/tests.py
@@ -6,31 +6,29 @@ from django.test import TestCase
 class CartTests(TestCase):
     def test_error_mail01(self):
         user = {'CustomerName':'jeff', 'CustomerEmail': '123','CustomerAddress': 'FCU', 'CustomerPhone': '0231513254'}
-        r = self.client.post('/cartok/', user)
-         
+        r = self.client.post('/cartok/', user)         
         self.assertEqual(r.status_code, 302)
         self.assertEqual(r.url, '/cartorder/')
+
     def test_error_mail02(self):
         user = {'CustomerName':'jeff', 'CustomerEmail': 'sdfsdfsdfsdf','CustomerAddress': 'FCU', 'CustomerPhone': '0231513254'}
-        r = self.client.post('/cartok/', user)
-         
+        r = self.client.post('/cartok/', user)         
         self.assertEqual(r.status_code, 302)
         self.assertEqual(r.url, '/cartorder/')
+        
     def test_error_mail03(self):
         user = {'CustomerName':'jeff', 'CustomerEmail': 'dfsdf@2f56.156.254','CustomerAddress': 'FCU', 'CustomerPhone': '0231513254'}
-        r = self.client.post('/cartok/', user)
-         
+        r = self.client.post('/cartok/', user)         
         self.assertEqual(r.status_code, 200)
         
     def test_error_mail04(self):
-        user = {'CustomerName':'jeff', 'CustomerEmail': 'dfsdf2f56.156.254','CustomerAddress': 'FCU', 'CustomerPhone': '0231513254'}
-        r = self.client.post('/cartok/', user)
-         
+        user = {'CustomerName':'jeff', 'CustomerEmail': 'dfsdf2f56.156.254','CustomerAddress': 'FCU', 'CustomerPhone': '02315132'}
+        r = self.client.post('/cartok/', user)         
         self.assertEqual(r.status_code, 302)
         self.assertEqual(r.url, '/cartorder/')
+
     def test_error_mail05(self):
         user = {'CustomerName':'jeff', 'CustomerEmail': 'dasd@dad.com','CustomerAddress': 'FCU', 'CustomerPhone': '0231513254'}
-        r = self.client.post('/cartok/', user)
-         
+        r = self.client.post('/cartok/', user)         
         self.assertEqual(r.status_code, 200)
         
